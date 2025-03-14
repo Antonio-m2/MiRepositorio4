4ced12a4f9b87dbeb7a47a8112c0f114d229504d
Antonio Moreno <antoniomorenomiranda17@gmail.com>
2025-03-14 01:26:55 +0000

    Practica4

--- appSistem4/src/app/app.module.ts
+++ appSistem4/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardproductoComponent } from './components/cardproducto/cardproducto.component';
+import { CardproductosComponent } from './components/cardproductos/cardproductos.component';
+import { CardproductoTarjetaComponent } from './components/cardproducto-tarjeta/cardproducto-tarjeta.component';
+import { CardbuscadorComponent } from './components/cardbuscador/cardbuscador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AboutComponent,
+    NavbarComponent,
+    FooterComponent,
+    CardproductoComponent,
+    CardproductosComponent,
+    CardproductoTarjetaComponent,
+    CardbuscadorComponent
   ],
   imports: [
     BrowserModule,
